241900a8307b0d3ab683d2bb8c613857b8f306ea
tobam <tobamohaqqeq84@gmail.com>
2025-11-07 23:12:02 +0000

    4th

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
-#include <iostream>
+//Toba Mohaqqeq 
+//COMSC-210
+//LAB-29
 
-int main() { std::cout << "Hello World!\n"; }
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <array>
+#include <list>
+#include <iomanip>
+
+using namespace std;
+
+//-------stages and types--------
+ enum Stage {SEEDIN = 0, GROWING = 1, MATURE = 2};
+using Buckets = array <list<string>, 3>;
+using Env = map<string, Buckets>;
+
+//convert stage string to enum 
+int stage_from_string(const string& s){
+    
+}
